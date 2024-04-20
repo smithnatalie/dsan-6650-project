8acdee67f2e5c1c3351e2abf45f78031407d02c7
Natalie Smith <nls73@georgetown.edu>
2024-04-20 16:26:03 -0400

    forest map generator - for creating multiple maps

---
+++ envs/forest_map_generator.py
@@ -0,0 +1,22 @@
+import os
+from envs.forest_view import Maps
+
+if __name__ == "__main__":
+    
+    #check that map_options exists in cwd
+    dir_name = os.path.join(os.getcwd(), "map_options")
+
+    #may remove or change option
+    #create new map by incrementing to play unique map state games - starting with 100 possible map options
+    map_path = None
+    for i in range(1, 100):
+        map_name = "map_%03d.npy" % i
+        map_path = os.path.join(dir_name, map_name)
+        if not os.path.exists(map_path):
+            break
+        if i == 99:
+            raise ValueError("Total number of possible maps in directory (100) exceeded.")
+        
+    maps = Maps(map_size=(10,10))
+    map.save_map(map_path)
+    print("A new map has been generated. It is located at %s." % map_path)
