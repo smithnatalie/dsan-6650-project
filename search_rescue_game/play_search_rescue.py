4b12d4acf3a041e9cb360c77e22042c12b1fed2e
Natalie Smith <nls73@georgetown.edu>
2024-04-22 22:58:17 -0400

    modularizing code, easier to read

--- search_rescue_game/play_search_rescue.py
+++ search_rescue_game/play_search_rescue.py
@@ -2,7 +2,22 @@
 #then the results are plotted together
 
 import matplotlib.pyplot as plt
+from search_rescue_tabular_q import run_tabular_q_learning
 
 #SHALLOW RL - Tabular Q Learning
-def run_tabular_q_learning():
-    from search_rescue_tabular_q import run
+    
+def plot_results(tabular_rewards, deep_rewards):
+    plt.figure(figsize=(10, 5))
+    plt.plot(tabular_rewards, label='Tabular Q-Learning Rewards')
+    plt.plot(deep_rewards, label='Deep Q-Learning Rewards')
+    plt.xlabel('Episode')
+    plt.ylabel('Total Reward')
+    plt.title('Comparison of Tabular and Deep Q-Learning Rewards')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+if __name__ == "__main__":
+    tabular_rewards = run_tabular_q_learning()
+    # deep_rewards = run_deep_q_learning()
+    plot_results(tabular_rewards)#, deep_rewards)
--- search_rescue_game/search_rescue_tabular_q.py
+++ search_rescue_game/search_rescue_tabular_q.py
@@ -16,13 +16,6 @@ from gymnasium.wrappers import RecordEpisodeStatistics
 import matplotlib.pyplot as plt
 
 
-# sys.path.append('/Users/smithnatalie/Desktop/dsan-6650-project')
-
-#debugging
-# print("Registered environments:", [spec.id for spec in gymnasium.envs.registry.values()])
-
-###
-
 #folder where we defined classes, map rendering, etc. 
 
 #searchable learning rate and options:
@@ -30,107 +23,159 @@ import matplotlib.pyplot as plt
 #felt like i needed something more comprehensive than a list like [0.1, 0.2, ] etc
 # https://github.com/monokim/PyRacing/blob/master/Pyrace_RL.py
 
-def calc_learning_rate(t: int) -> float:
-    #this code will allow us to search through multiple possible rates + decay of reward over time
+# def calc_learning_rate(t: int) -> float:
+#     #this code will allow us to search through multiple possible rates + decay of reward over time
+#     return max(0.2, min(0.8, 1.0 - math.log10((t + 1) / reward_decay)))
+
+# def calc_exploration_rate(t: int) -> float:
+#     return max(0.001, min(0.8, 1.0 - math.log10((t + 1) / reward_decay)))
+
+# if __name__ == "__main__":
+#     #create environment - this map will change depending on which want to select
+#     #start with a random generation
+#     env = gymnasium.make("map-random-10x10-v0", render_mode = "rgb_array")
+#     #debugging
+#     print("Render mode of the environment:", env.metadata.get('render_modes'))
+#     print("Environment metadata:", env.metadata)
+
+#     #screen recording
+#     recording_enabled = True
+    
+#     #source for how to set up gymnasium record video and episode statistics
+#     # https://gymnasium.farama.org/main/api/wrappers/misc_wrappers/#gymnasium.wrappers.RecordVideo
+    
+#     #recording every 10th episode -  used ChatGPT to help me do this
+    
+#     print("Render mode of the environment before RecordVideo:", env.metadata.get('render_modes'))
+
+#     if recording_enabled:    
+#         env = RecordVideo(env, video_folder='./game_recordings', episode_trigger=lambda x: x % 10 == 0, name_prefix="video")
+
+#     map_size: Tuple[int,int] = tuple((env.observation_space.high + np.ones(env.observation_space.shape)).astype(int))
+
+#     #parameters: will change based on needs
+    
+#     #training
+#     num_episodes: int = 50
+#     num_episode_steps: int = np.prod(map_size, dtype=int) * 100
+#     num_actions: int = env.action_space.n
+    
+#     #learning
+#     reward_decay = float = np.prod(map_size, dtype=float) / 10.0
+#     learning_rate: float = calc_learning_rate(0)
+#     exploration_rate: float = calc_exploration_rate(0)
+#     discount_factor: float = 0.99
+    
+    
+#     #Q values
+#     # Q: np.ndarray = np.zeros(map_size + (num_actions,), dtype=float)
+#     Q = np.zeros(map_size + (num_actions,), dtype=float)
+    
+#     rewards = []
+    
+#     for episode in range(num_episodes):
+#         total_reward = 0
+#         observation, info = env.reset()
+#         current_state = tuple(observation)
+
+#         for episode_step in range(num_episode_steps):
+#             # how the dog agent will select the action to take next
+#             # random action based on exploration rate 
+#             action = int(env.action_space.sample()) if random.uniform(0,1) < exploration_rate else int(np.argmax(Q[current_state]))
+            
+#             #going back to what was defined in forest_env for step function
+#             #should require 5 inputs
+#             observation, reward, terminated, truncated, _  = env.step(action)
+            
+#             total_reward += reward
+            
+#             #go to next state
+#             next_state = tuple(observation)
+            
+#             #temporal difference error
+#             TD = reward + discount_factor * np.amax(Q[next_state]) - Q[current_state + (action,)]
+            
+#             #bellman equation :-)
+    
+#             Q[current_state + (action,)] += learning_rate * TD
+            
+#             current_state = next_state
+            
+#             #end of game - reaches goal - terminated = true
+#             if terminated:
+#                 print(f"Episode {episode + 1}/{num_episodes} complete after {episode_step + 1} steps. Total reward = {total_reward}.")
+#                 break
+#             #if reach max steps without goal
+#             elif episode_step >= num_episode_steps - 1:
+#                 print(f"Episode {episode + 1}/{num_episodes} timed out after {episode_step + 1} steps. Total reward = {total_reward}.")
+                
+        
+#         #updating parameters
+#         rewards.append(total_reward)
+#         exploration_rate = calc_exploration_rate(episode)
+#         learning_rate = calc_learning_rate(episode)
+        
+#     env.close()
+                
+                
+# #Plotting Rewards
+
+# plt.plot(rewards)
+# plt.title('Total Rewards per Episode')
+# plt.xlabel('Episode')
+# plt.ylabel('Total Reward')
+# plt.show()
+#~>~>~>~>~>~>~>>~>~>~>~>~>~>~>~>~>~>~>~>~>~>~>~>~
+
+#TESTING
+
+
+def calc_learning_rate(t: int, reward_decay):
     return max(0.2, min(0.8, 1.0 - math.log10((t + 1) / reward_decay)))
 
-def calc_exploration_rate(t: int) -> float:
+def calc_exploration_rate(t: int, reward_decay):
     return max(0.001, min(0.8, 1.0 - math.log10((t + 1) / reward_decay)))
 
-
-if __name__ == "__main__":
-    #create environment - this map will change depending on which want to select
-    #start with a random generation
-    env = gymnasium.make("map-random-10x10-v0", render_mode = "rgb_array")
-    #debugging
-    print("Render mode of the environment:", env.metadata.get('render_modes'))
-    print("Environment metadata:", env.metadata)
-
-    #screen recording
+def run_tabular_q_learning():
+    env = gymnasium.make("map-random-10x10-v0", render_mode="rgb_array")
     recording_enabled = True
     
-    #source for how to set up gymnasium record video and episode statistics
-    # https://gymnasium.farama.org/main/api/wrappers/misc_wrappers/#gymnasium.wrappers.RecordVideo
-    
-    #recording every 10th episode -  used ChatGPT to help me do this
-    
-    print("Render mode of the environment before RecordVideo:", env.metadata.get('render_modes'))
-
-    if recording_enabled:    
+    if recording_enabled:
         env = RecordVideo(env, video_folder='./game_recordings', episode_trigger=lambda x: x % 10 == 0, name_prefix="video")
 
-    map_size: Tuple[int,int] = tuple((env.observation_space.high + np.ones(env.observation_space.shape)).astype(int))
-
-    #parameters: will change based on needs
-    
-    #training
-    num_episodes: int = 50
-    num_episode_steps: int = np.prod(map_size, dtype=int) * 100
-    num_actions: int = env.action_space.n
-    
-    #learning
-    reward_decay = float = np.prod(map_size, dtype=float) / 10.0
-    learning_rate: float = calc_learning_rate(0)
-    exploration_rate: float = calc_exploration_rate(0)
-    discount_factor: float = 0.99
-    
-    
-    #Q values
-    # Q: np.ndarray = np.zeros(map_size + (num_actions,), dtype=float)
+    map_size = tuple((env.observation_space.high + np.ones(env.observation_space.shape)).astype(int))
+    num_episodes = 50
+    num_episode_steps = np.prod(map_size, dtype=int) * 100
+    num_actions = env.action_space.n
+    reward_decay = np.prod(map_size, dtype=float) / 10.0
     Q = np.zeros(map_size + (num_actions,), dtype=float)
-    
     rewards = []
-    
+
     for episode in range(num_episodes):
         total_reward = 0
         observation, info = env.reset()
         current_state = tuple(observation)
 
         for episode_step in range(num_episode_steps):
-            # how the dog agent will select the action to take next
-            # random action based on exploration rate 
+            exploration_rate = calc_exploration_rate(episode, reward_decay)
+            learning_rate = calc_learning_rate(episode, reward_decay)
             action = int(env.action_space.sample()) if random.uniform(0,1) < exploration_rate else int(np.argmax(Q[current_state]))
-            
-            #going back to what was defined in forest_env for step function
-            #should require 5 inputs
-            observation, reward, terminated, truncated, _  = env.step(action)
-            
+            observation, reward, terminated, truncated, _ = env.step(action)
             total_reward += reward
-            
-            #go to next state
             next_state = tuple(observation)
-            
-            #temporal difference error
-            TD = reward + discount_factor * np.amax(Q[next_state]) - Q[current_state + (action,)]
-            
-            #bellman equation :-)
-    
+            TD = reward + 0.99 * np.amax(Q[next_state]) - Q[current_state + (action,)]
             Q[current_state + (action,)] += learning_rate * TD
-            
             current_state = next_state
-            
-            #end of game - reaches goal - terminated = true
-            if terminated:
+
+            if terminated or episode_step >= num_episode_steps - 1:
                 print(f"Episode {episode + 1}/{num_episodes} complete after {episode_step + 1} steps. Total reward = {total_reward}.")
                 break
-            #if reach max steps without goal
-            elif episode_step >= num_episode_steps - 1:
-                print(f"Episode {episode + 1}/{num_episodes} timed out after {episode_step + 1} steps. Total reward = {total_reward}.")
-                
-        
-        #updating parameters
-        rewards.append(total_reward)
-        exploration_rate = calc_exploration_rate(episode)
-        learning_rate = calc_learning_rate(episode)
-        
-    env.close()
-                
-                
-#Plotting Rewards
 
-plt.plot(rewards)
-plt.title('Total Rewards per Episode')
-plt.xlabel('Episode')
-plt.ylabel('Total Reward')
-plt.show()
-#~>~>~>~>~>~>~>>~>~>~>~>~>~>~>~>~>~>~>~>~>~>~>~>~
+        rewards.append(total_reward)
+    
+    env.close()
+    return rewards
+
+if __name__ == "__main__":
+    run_tabular_q_learning()
+
